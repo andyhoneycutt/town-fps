6966c5f89bae4ddc29757fe28518d7a54a6a333a
Andy Honeycutt <ahoneycutt@gmail.com>
2025-11-07 13:25:35 -0700

    add bricks & brick count detection

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 3.0
 @export var jump_velocity = 4.5
+@export var bricks = 10
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
